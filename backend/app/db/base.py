5d168033272082a1a04fea61bf56aed18d06aeed
Erfan <260531016+ERFworks@users.noreply.github.com>
2026-07-21 21:47:09 +0330

    feat(db): register all models in base

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,1 +1,7 @@
 from app.db.base_class import Base
+from app.models.user import User
+from app.models.profile import Profile       
+from app.models.friendship import Friendship  
+from app.models.conversation import Conversation                    
+from app.models.conversationparticipant import ConversationParticipant  
+from app.models.message import Message  
