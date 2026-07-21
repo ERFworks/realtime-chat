13d5424675c2a26b12cbe7911b78ad425b51eaad
Erfan <260531016+ERFworks@users.noreply.github.com>
2026-07-21 12:03:04 +0330

    feat(models): implement profile model

---
+++ backend/app/models/profile.py
@@ -0,0 +1,16 @@
+from app.models.user import utcnow
+from app.db.base_class import Base
+from datetime import datetime
+from sqlalchemy import String, Integer, DateTime, ForeignKey, Text
+from sqlalchemy.orm import Mapped ,mapped_column, relationship
+
+
+class Profile(Base):
+    __tablename__ = "profiles"
+
+    profile_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    biography: Mapped[str | None] = mapped_column(Text, nullable=True)
+    profile_pic: Mapped[str | None] = mapped_column(String(255), nullable=True)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=utcnow, onupdate=utcnow)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"), unique=True, nullable=False)
+    user: Mapped["User"] = relationship(back_populates="profile")
