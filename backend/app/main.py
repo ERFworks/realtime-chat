b5bcf11eb46a68cc77ffb947949942b31022ce90
Erfan <260531016+ERFworks@users.noreply.github.com>
2026-07-25 15:25:06 +0330

    feat(messages): add send message and get messages endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.api.v1 import auth, conversations
+from app.api.v1 import auth, conversations, messages
 
 from app.db import base
 
@@ -8,6 +8,7 @@ app = FastAPI(title="Raeltime Chat App")
 
 app.include_router(auth.router, prefix="/api/v1/auth")
 app.include_router(conversations.router, prefix="/api/v1/conversations")
+app.include_router(messages.router, prefix="/api/v1/conversations")
 
 @app.get("/health")
 async def health():
