7ea7ffa64f9aa2f8fe6913440547baca5014e4bf
Erfan <260531016+ERFworks@users.noreply.github.com>
2026-08-02 21:00:10 +0330

    refactor(repository): convert profile  repositories to classes with Protocol interfaces

--- backend/app/repositories/profile.py
+++ backend/app/repositories/profile.py
@@ -1,24 +1,36 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from typing import Protocol
 
 from app.models.profile import Profile
 
-async def create_profile(db: AsyncSession, user_id: int) -> Profile:
-    profile = Profile(
-        user_id = user_id,
-        biography = None,
-        profile_pic = None,
-    )
 
-    db.add(profile)
-    await db.flush()
-
-    return profile
+class AbstractProfileRepository(Protocol):
+    async def create_profile(self, user_id: int) -> Profile: ...
+    async def get_profile_by_user_id(self, user_id: int) -> Profile | None: ...
 
 
-async def get_profile_by_user_id(db: AsyncSession, user_id: int) -> Profile | None:
-    stmt = select(Profile).where(Profile.user_id == user_id)
+class SqlAlchemyProfileRepository:
 
-    result = await db.execute(stmt)
-    return result.scalar_one_or_none()
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def create_profile(self, user_id: int) -> Profile:
+        profile = Profile(
+            user_id = user_id,
+            biography = None,
+            profile_pic = None,
+        )
+
+        self.session.add(profile)
+        await self.session.flush()
+
+        return profile
+
+
+    async def get_profile_by_user_id(self, user_id: int) -> Profile | None:
+        stmt = select(Profile).where(Profile.user_id == user_id)
+
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
 
